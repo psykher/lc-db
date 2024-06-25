dd11ad1c2ec7eb5e5069a1eef9b62a2c316f301e
Carlos Alvarez del Castillo Garcia <psykher@gmail.com>
2024-06-24 22:00:21 -0600

    Added 2236

---
+++ 2236/python/2236.py
@@ -0,0 +1,20 @@
+# python 2236.py
+
+class TreeNode:
+    def __init__(self, val, right, left):
+        self.val = val
+        self.right = right
+        self.left = left
+
+
+class Solution(object):
+    def checkTree(self, root):
+        eSum = (root.left.val | 0) + (root.right.val | 0)
+
+        return True if (root.val == eSum) else False
+
+solution = Solution()
+
+treeNode = TreeNode(5, TreeNode(3, 0, 0), TreeNode(1, 0, 0))
+
+print(solution.checkTree(treeNode))
